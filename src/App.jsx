f64a1a3ea719c899d2848a06e8fceb240715b446
Jessica Martinez <aleejess@gmail.com>
2024-09-04 21:01:40 -0500

    add react-router-dom and stablish the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import { routes } from "./Components/utils/routes";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
+import Detail from "./Routes/Detail";
 
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
+          <Routes>
+            <Route path={routes.home} element={<Home />} />
+            <Route path={routes.contact} element={<Contact />} />
+            <Route path={routes.favs} element={<Favs />} />
+            <Route path="/detail/:id" element={<Detail />} />
+          </Routes>
+
           <Footer/>
       </div>
   );
